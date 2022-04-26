2f35a7eee6883207564fda64718970ec1679065b
Nithyadevi <nithi2218@gmail.com>
2022-04-26 23:03:10 +1000

    fetch done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # currency-converter
+
+
+https://www.ups.com/worldshiphelp/WSA/ENG/AppHelp/mergedProjects/CORE/Codes/Country_Territory_and_Currency_Codes.htm
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const selectFromList = document.querySelectorAll("form select");
-const getButton = document.querySelector("form button");
+const selectFromList = document.querySelectorAll("select");
+const getButton = document.querySelector("button");
 const fromCurrency = document.querySelector(".from select");
 const toCurrency = document.querySelector(".to select");
+const amount = document.querySelector("input");
+const exchangeRateTxt = document.querySelector(".exchange-rate");
 
 
 for (let i = 0; i < selectFromList.length; i++) {
@@ -28,7 +30,30 @@ getButton.addEventListener("click", e =>{
     getExchangeRate();
 });
 
+const exchangeIcon = document.querySelector(".icon");
+exchangeIcon.addEventListener("click", ()=>{
+    let tempCode = fromCurrency.value;
+    fromCurrency.value = toCurrency.value;
+    toCurrency.value = tempCode;
+    getExchangeRate();
+})
 
-function getExchangeRate(){
 
+function getExchangeRate() {
+       
+    let amountVal = amount.value;
+    if(amountVal == "" || amountVal == "0"){
+        amount.value = "1";
+        amountVal = 1;
+    }
+    exchangeRateTxt.innerText = "Getting...";
+    let url = `https://v6.exchangerate-api.com/v6/8ab27104b2e165a428af6d21/latest/${fromCurrency.value}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(response =>{
+        let exchangeRate = response.conversion_rates[toCurrency.value];
+        let totalExRate = (amountVal * exchangeRate);
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+    });
 }
+
